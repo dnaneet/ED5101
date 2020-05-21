bfa5cb33b35a8483ac02acfcb3b3dd7155309d0b
Aneet D Narendranath, PhD <dnaneet@mtu.edu>
2020-05-21 09:42:02 -0400

    Update script_ed5101_2.js

--- ProvingGrounds/script_ed5101_2.js
+++ ProvingGrounds/script_ed5101_2.js
@@ -10,7 +10,7 @@ $(document).ready(function() {
   var POINT_Y_PREFIX = ''; // prefix for x values, eg 'USD '
   var POINT_Y_POSTFIX = ''; // postfix for x values, eg ' kg'
 
-  var POINT_NAME = 'taskString'; // point names that appear in tooltip
+  var POINT_NAME = 'cumulative'; // point names that appear in tooltip
   var POINT_COLOR = 'rgba(0,0,255,0.7)'; // point color, eg `black` or `rgba(10, 100, 44, 0.8)`
   var POINT_RADIUS = 3; // radius of each data point
 
